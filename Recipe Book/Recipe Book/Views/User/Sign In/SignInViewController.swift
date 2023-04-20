9689cee2f57430a344262050883dc09c6bea818d
Chase <chaseaohill@gmail.com>
2023-04-20 18:31:36 -0400

    Successfully signed into an account using Apple ID.

--- Recipe Book/Recipe Book/Views/User/Sign In/SignInViewController.swift
+++ Recipe Book/Recipe Book/Views/User/Sign In/SignInViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import AuthenticationServices
 import FirebaseAuth
+import CryptoKit
 
 class SignInViewController: UIViewController {
     
@@ -17,6 +18,7 @@ class SignInViewController: UIViewController {
     
     // MARK: - Properties
     var viewModel: SignInViewModel!
+    var nonce: String?
     
     // MARK: - LifeCycle
     override func viewDidLoad() {
@@ -34,6 +36,7 @@ class SignInViewController: UIViewController {
         signInWithAppleButton.addTarget(self, action: #selector(appleSignInButtonTapped), for: .touchUpInside)
     }
     
+    // MARK: - Functions
     @objc func appleSignInButtonTapped() {
         let provider = ASAuthorizationAppleIDProvider()
         let request = provider.createRequest()
@@ -41,10 +44,56 @@ class SignInViewController: UIViewController {
             .email,
             .fullName
         ]
+        
+        self.nonce = randomNonceString()
+        request.nonce = sha256(nonce!)
+        
         let authController = ASAuthorizationController(authorizationRequests: [request])
         authController.presentationContextProvider = self
         authController.delegate = self
         authController.performRequests()
+    }
+    
+    private func randomNonceString(length: Int = 32) -> String {
+        precondition(length > 0)
+        let charset: Array<Character> =
+            Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+        var result = ""
+        var remainingLength = length
+        
+        while remainingLength > 0 {
+            let randoms: [UInt8] = (0 ..< 16).map { _ in
+                var random: UInt8 = 0
+                let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+                if errorCode != errSecSuccess {
+                    fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+                }
+                return random
+            }
+            
+            randoms.forEach { random in
+                if remainingLength == 0 {
+                    return
+                }
+                
+                if random < charset.count {
+                    result.append(charset[Int(random)])
+                    remainingLength -= 1
+                }
+            }
+        }
+        
+        return result
+    }
+
+    private func sha256(_ input: String) -> String {
+        let inputData = Data(input.utf8)
+        let hashedData = SHA256.hash(data: inputData)
+        let hashString = hashedData.compactMap {
+            return String(format: "%02x", $0)
+        } .joined()
+        
+        return hashString
     }
     
     // MARK: - Actions
@@ -84,10 +133,25 @@ extension SignInViewController: ASAuthorizationControllerPresentationContextProv
 
 extension SignInViewController: ASAuthorizationControllerDelegate {
     func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
-        <#code#>
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            // Do something with this credential
+            guard let nonce = self.nonce else {
+                fatalError()
+            }
+            guard let appleIDToken = appleIDCredential.identityToken else {
+                print("Failed to get Apple Token") ; return
+            }
+            guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+                print("Failed to get id Token") ; return
+            }
+            let firebaseCredential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: nonce)
+            Auth.auth().signIn(with: firebaseCredential) { [weak self] result, error in
+                //Do something after Firebase sign in has completed
+            }
+        }
     }
     
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        <#code#>
+        
     }
 }
